bbfc99b09744d790264f270137cdeb774a1ad02a
Shah Nawaz Khan <blooshaanster@gmail.com>
2020-11-13 13:31:01 +0530

    grey'd out

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { hot } from 'react-hot-loader/root';
 import './styles/tailwind.css';
 import './styles/main.css';
 
 const App = () => {
+  const [state, setState] = useState({ xAxis: 0, yAxis: 0 });
+  const handleMouseMove = (e) => {
+    const xAxis = (window.innerWidth / 2 - e.pageX) / -20;
+    const yAxis = (window.innerHeight / 2 - e.pageY) / 20;
+    setState({ xAxis, yAxis });
+  };
+  console.log('rendered');
   return (
-    <div className="relative">
-      <nav className="sticky top-0 z-50 shadow-md bg-pink-200 p-5">
-        <h1 className="text-2xl font-bold">React Starter</h1>
-      </nav>
-      <div className="hero">
-        <img
-          src="https://images.unsplash.com/photo-1480506132288-68f7705954bd?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1993&q=80"
-          alt="hero"
-        />
-      </div>
-      <section className="canister">
-        <h1 className="text-xl font-semibold">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto
-          ullam corrupti molestias veniam doloremque enim ex atque provident
-          voluptas necessitatibus voluptatibus, culpa dicta repudiandae,
-          corporis velit et molestiae reprehenderit recusandae.
+    <div
+      className="relative min-h-screen flex items-center justify-center"
+      // onMouseMove={handleMouseMove}
+    >
+      <div
+        className="p-10 absolute left-0 bottom-0"
+        style={{
+          transform: `rotateY(${state.xAxis}deg) rotateX(${state.yAxis}deg)`,
+          transition: 'transform 0.3s ease',
+        }}
+      >
+        <h1 className="font-bold text-6xl">
+          HI IM SHNK, <br /> I MAKE WEBSITES
         </h1>
-      </section>
+      </div>
     </div>
   );
 };
